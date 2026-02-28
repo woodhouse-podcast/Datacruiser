edb52e2f90361cff7f4d9c475c11d9468757ea94
Ryan <ryan@deathridge.io>
2026-02-28 22:43:10 +1100

    Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/datacruiser-website/', // Replace with your GitHub repo name
+  base: '/Datacruiser/', // GitHub repo name
 })
